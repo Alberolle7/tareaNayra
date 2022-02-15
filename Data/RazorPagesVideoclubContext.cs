b677c7e8a2f9c25a9f3bfe63bb456bc583f1733c
Alberolle7 <Alberolle7@gmail.com>
2022-02-15 02:07:49 +0100

    hecho

---
+++ Data/RazorPagesVideoclubContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesVideoclub.Models;
+
+namespace RazorPagesVideoclub.Data
+{
+    public class RazorPagesVideoclubContext : DbContext
+    {
+        public RazorPagesVideoclubContext (DbContextOptions<RazorPagesVideoclubContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesVideoclub.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesVideoclub.Data;
+using RazorPagesVideoclub.Models;
+
+namespace RazorPagesVideoclub.Pages.Admin
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesVideoclub.Data.RazorPagesVideoclubContext _context;
+
+        public IndexModel(RazorPagesVideoclub.Data.RazorPagesVideoclubContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
